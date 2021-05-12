56eb9ce8d3d71b36c5794a62f05c982c05e9343a
lovepreett-89 <lovishkaur.88@gmail.com>
2021-05-12 20:09:02 +0530

    assignment commit

---
+++ assignment.r
@@ -0,0 +1,38 @@
+# basic arithmetic functions
+# addition
+3+6
+1+4
+46+98
+# substraction
+7-4
+19-16
+7-9
+# square root
+sqrt(16)
+sqrt(78)
+# exponents
+13^2
+2^3
+# multipication
+8*2
+4*5
+# mean
+mean(1,10,100) # result should be 37
+median(1,10,100) # result should be 10
+median(1,10,100)
+?median
+(c(1,10,100)) # to store our series of values
+mean(c(1,10,100))
+mean(20,40,80)
+median(20,40,80)
+mode(c(20,40,80))
+mean(c(20,40,80))
+median(c(20,40,80))
+mode(c(20,40,80))
+s# to store the series of number in a variable = <-
+a = (c(12,14,15))
+a<-(c(45,67,89))
+a
+a
+mean(a)
+median(a)
